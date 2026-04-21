73bd4a3537b8dac05e386bbdf39437f2e5a8e3fa
samuelhuanca-eng <samuel.huanca@workwave.com>
2026-04-21 16:29:25 -0400

    initial builr in types

--- BackendLessons/BackendLessons/Program.cs
+++ BackendLessons/BackendLessons/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double largeValue = 1f; // One million
+            double smallValue = 0.1f;      // One tenth
+
+            double result = largeValue;
+
+            for (int i = 0; i < 10; i++)
+            {
+                result += smallValue;
+            }
+            //1000001.0f
+            Console.WriteLine(result);
         }
     }
 }
--- BackendLessons/BackendLessonsModern/Program.cs
+++ BackendLessons/BackendLessonsModern/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/// Top level 
+
+//// 1. .Net and Visual Studio
+
+
+
+
+
+
+Console.WriteLine("Hello, World Modern!");

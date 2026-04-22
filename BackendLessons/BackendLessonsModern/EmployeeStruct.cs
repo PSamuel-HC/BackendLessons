2ae97bac898d4edd607334c589521969b5796968
samuelhuanca-eng <samuel.huanca@workwave.com>
2026-04-22 16:53:08 -0400

    value and reference types and homework

---
+++ BackendLessons/BackendLessonsModern/EmployeeStruct.cs
@@ -0,0 +1,24 @@
+﻿namespace BackendLessonsModern
+{
+    struct EmployeeStruct
+    {
+        // Fields
+        private int _age;
+
+        // propiedad
+        public bool IsDeleted { get; set; }
+        public string Name { get; set; }
+
+        public EmployeeStruct(int age, string name)
+        {
+            _age = age;
+            Name = name;
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine($"Employee: {Name}, age: {_age}, isDeleted: {IsDeleted}");
+        }
+
+    }
+}
---
+++ BackendLessons/BackendLessonsModern/WorkerEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendLessonsModern
+{
+    enum WorkerEnum
+    {
+        QA,             // 0
+        Dev = 8,        // 8
+        Manager         // 9
+    }
+}

069962b176248883e4bc025b7bae168c72a69237
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-04-22 18:39:58 -0500

    Updating program with all the bugs fixed

--- BackendLessons/BrokenCode/Program.cs
+++ BackendLessons/BrokenCode/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace BrokenCode
+﻿using System.Text.Json;
+
+namespace BrokenCode
 {
     public class UserProfile
     {
-        public string UserName { get; set; }
-        public string Rank { get; set; }
+        public required string UserName { get; set; } // added 'required' to avoid warning -> Non-nullable property 'UserName' must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring the property as nullable.
+        public required string Rank { get; set; }
     }
 
     public struct BankTransaction
@@ -17,35 +19,51 @@
         static void Main(string[] args)
         {
             UserProfile primaryUser = new UserProfile { UserName = "Alice", Rank = "Gold" };
-            UserProfile backupUser = primaryUser; // BUG: Reference Copy
+            //UserProfile backupUser = primaryUser; // BUG: Reference Copy
+            /* Previous line will only create another pointer to the original object. To create a
+            Deep copy of the object, nowadays one of the methods is to serialize and
+            deserialize the original object. Serializing creates a JSON object based on the original
+            object (like extracting its data), and the deserializing it is to create a new
+            object with that JSON */
+            string backupUser1 = JsonSerializer.Serialize(primaryUser);
+            Console.WriteLine($"backupUser1 {backupUser1}");
+            UserProfile backupUser = JsonSerializer.Deserialize<UserProfile>(JsonSerializer.Serialize(primaryUser))!;
 
             backupUser.UserName = "Bob";
 
             Console.WriteLine($"Primary User: {primaryUser.UserName}");
-            // Bug #1
-            // Expecting "Alice", but it will be "Bob".
+            Console.WriteLine($"Backup User: {backupUser.UserName}");
+            // Bug #1 SOLVED!
+            // Expecting "Alice", but it will be "Bob". 
 
 
             BankTransaction currentTx = new BankTransaction { Amount = 100.00m, IsVerified = false };
 
-            ProcessPayment(currentTx);
+            currentTx = ProcessPayment(currentTx);
 
             Console.WriteLine($"Final Balance: {currentTx.Amount}, Verified: {currentTx.IsVerified}");
-            // Bug #2
+            // Bug #2 SOLVED!
             // Expecting 150.00 and True, but will be 100.00 and False.
+            /* This error occurs because BankTransaction is a struct, which means, a value data type.
+             * So the only way to assign a value that comes from another funtion to this scruct is
+             * to have the function return the value and assign it to currentTx */
 
 
-            // Bug #3
+            // Bug #3 SOLVED!
             // Program Crashes
-            UserProfile guestUser = null;
+            /* The function PrintReport is trying to access a non existing property of guestUser because
+             * it is pointing to null. To solve this we basically have to actually create a UserProfile
+             * object with at least (although both fiels are required now) UserName */
+            UserProfile guestUser = new UserProfile { UserName = "Juan", Rank = "Black" };
             PrintReport(guestUser);
         }
 
-        static void ProcessPayment(BankTransaction tx)
+        static BankTransaction ProcessPayment(BankTransaction tx)
         {
             tx.Amount += 50.00m;
             tx.IsVerified = true;
             Console.WriteLine("--- Internal: Payment processed successfully! ---");
+            return tx;
         }
 
         static void PrintReport(UserProfile profile)

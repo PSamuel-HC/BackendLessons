d9e001c2bfdd5732f1a1ad616e649eb1e8e561fc
German-Jala <German.Alvarez@jala.university>
2026-04-23 03:22:08 -0400

    Resolved 3 Bugs

--- BackendLessons/BrokenCode/Program.cs
+++ BackendLessons/BrokenCode/Program.cs
@@ -1,12 +1,13 @@
 ﻿namespace BrokenCode
 {
-    public class UserProfile
+    public struct UserProfile
     {
         public string UserName { get; set; }
         public string Rank { get; set; }
     }
 
-    public struct BankTransaction
+
+    public class BankTransaction
     {
         public decimal Amount;
         public bool IsVerified;
@@ -16,29 +17,65 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bug 1: Convert Class to Struct\n");
             UserProfile primaryUser = new UserProfile { UserName = "Alice", Rank = "Gold" };
             UserProfile backupUser = primaryUser; // BUG: Reference Copy
-
             backupUser.UserName = "Bob";
-
             Console.WriteLine($"Primary User: {primaryUser.UserName}");
             // Bug #1
             // Expecting "Alice", but it will be "Bob".
 
+            // ------------------------------------------
+            // ------------------------------------------
+            /*
+                I have changed the Type of USER PROFILE of Class to Struct
+                because I needed that the object needed to be stored in
+                Stack without any Heap Reference
+            */
+            // ------------------------------------------
+            // ------------------------------------------
+            Console.WriteLine("\n\n\n");
 
+
+
+
+            Console.WriteLine("Bug 2: Convert Struct to Class\n");
             BankTransaction currentTx = new BankTransaction { Amount = 100.00m, IsVerified = false };
-
             ProcessPayment(currentTx);
-
             Console.WriteLine($"Final Balance: {currentTx.Amount}, Verified: {currentTx.IsVerified}");
             // Bug #2
             // Expecting 150.00 and True, but will be 100.00 and False.
 
+            // ------------------------------------------
+            // ------------------------------------------
+            /*
+                I have changed the Type of BANK TRANSACTION of Struct to Class
+                Because I needed that the object conserves its reference saving
+                it in Heap Memory
+            */
+            // ------------------------------------------
+            // ------------------------------------------
+            Console.WriteLine("\n\n\n");
 
+
+
+            Console.WriteLine("Bug 3: Nullable Elements\n");
             // Bug #3
             // Program Crashes
-            UserProfile guestUser = null;
+            UserProfile? guestUser = null;
             PrintReport(guestUser);
+
+            // ------------------------------------------
+            // ------------------------------------------
+            /*
+                I change types with nullable options with ?
+                UserProfile? in the program Class and PrintReport function
+                PRINT REPORT function has been changed
+            */
+            // ------------------------------------------
+            // ------------------------------------------
+
+            Console.WriteLine("\n\n\n");
         }
 
         static void ProcessPayment(BankTransaction tx)
@@ -48,9 +85,9 @@
             Console.WriteLine("--- Internal: Payment processed successfully! ---");
         }
 
-        static void PrintReport(UserProfile profile)
+        static void PrintReport(UserProfile? profile)
         {
-            Console.WriteLine($"USER REPORT: {profile.UserName.ToUpper()}");
+            Console.WriteLine($"USER REPORT: { profile?.UserName?.ToUpper() ?? "Unknown Guest User"}");
         }
     }
 }

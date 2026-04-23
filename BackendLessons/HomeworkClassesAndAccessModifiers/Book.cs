09e62f3e3959108def6b76649b8f280945ec9cf5
Gero Le Lan <Geronimo.LeLan@jala.university>
2026-04-23 20:31:35 -0300

    feat: add Book class with checkout logic

---
+++ BackendLessons/HomeworkClassesAndAccessModifiers/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace HomeworkClassesAndAccessModifiers
+{
+    public sealed class Book
+    {
+        public string Title { get;}
+        private int _id;
+        private bool _isAvilable;
+
+        public Book (string title, int id)
+        {
+            Title = title;
+            _id = id;
+            _isAvilable = true;
+        }
+        public void Checkout() {
+            _isAvilable = !_isAvilable;
+        }
+        public void ShowDetails() {
+            Console.WriteLine($"Title:{Title}");
+            Console.WriteLine($"Available:{_isAvilable}");
+        }
+    }
+}
---
+++ BackendLessons/HomeworkClassesAndAccessModifiers/Program.cs
@@ -0,0 +1,11 @@
+﻿using HomeworkClassesAndAccessModifiers;
+using static System.Net.WebRequestMethods;
+
+Book book = new Book("The Title",123);
+
+Console.WriteLine("Before Chekout:");
+book.ShowDetails();
+Console.WriteLine("\n");
+book.Checkout();
+Console.WriteLine("After Chekout: ");
+book.ShowDetails();

64790bf86658d06c703c04845c54f765d2e09c08
bsalinasjala <bruno.salinas@jala.university>
2026-04-23 19:34:37 -0400

    feat: classes and access modifiers homework

---
+++ BackendLessons/ClassesAndAccessModifiers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassesAndAccessModifiers
+{
+    sealed class Book
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Book(string title, int id)
+        {
+            Title = title;
+            Id = id;
+            IsAvailable = true;
+        }
+
+        public void Checkout()
+        {
+            IsAvailable = false;
+        }
+
+        public void showDetails()
+        {
+            Console.WriteLine($"Book Id: {Id}");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Available: {IsAvailable}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("C# Basics",1);
+            Console.WriteLine("Before checkout");
+            book.showDetails();
+
+            Console.WriteLine();
+
+            book.Checkout();
+
+            Console.WriteLine("After checkout:");
+            book.showDetails();
+        }
+    }
+}

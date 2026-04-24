203e93b79d792cf82d826c03811f05350fd56260
RoBra18 <brand8n123@gmail.com>
2026-04-24 11:15:00 -0400

    Book Homework

---
+++ BackendLessons/MyBookHomework/Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBookHomework.Models
+{
+
+    sealed class Book //To prevent the class from being inherited, I have set its modifier to "sealed".
+    {
+        /*Since the id is sensitive, I am not allowing its 
+          modification (I have not placed "set") as it should not change*/
+        public int Id { get; }
+
+        public string Title { get; set; }
+
+        /*The default book availability is "true", I only allow its
+          modification within the class using "private set"*/
+        public bool IsAvailable { get; private set; } = true;
+
+
+        public Book(int _id, string _title)
+        {
+            Id = _id;
+            Title = _title;
+        }
+
+
+        internal void Checkout()
+        {
+            //I am verifying its availability.
+            if (!IsAvailable) throw new InvalidOperationException("Book is not available");
+            IsAvailable = false;
+        }
+
+        internal void ShowDetails()
+        {
+            Console.WriteLine($"Book Details:\nTitle: {Title}\nAvailable:: {IsAvailable}");
+        }
+    }
+}
---
+++ BackendLessons/MyBookHomework/Program.cs
@@ -0,0 +1,25 @@
+﻿using MyBookHomework.Models;
+
+try
+{
+    Console.WriteLine("My Book:");
+
+    Book myBook = new Book(1, "Juan & Juanita");
+    myBook.ShowDetails();
+
+    myBook.Checkout();
+
+    //Verifying changes
+    Console.WriteLine("My Book after checkout: ");
+    myBook.ShowDetails();
+
+    //Verifying validation:
+    Console.WriteLine("Validation: ");
+    myBook.Checkout();
+}
+
+catch (InvalidOperationException ex)
+{
+
+    Console.WriteLine(ex.Message);
+}

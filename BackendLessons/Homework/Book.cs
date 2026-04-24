aaf02fda0411a239ab8b1fdb0a4775378f2d6bdd
Luigi-Jala <luigi.cabrera@jala.university>
2026-04-23 22:47:59 -0500

    feat: Homework - classes-and-access-modifiers - Luigi Cabrera

---
+++ BackendLessons/Homework/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    // Using sealed in order to avoid other class to inherit from this class
+    internal sealed class Book
+    {
+
+        // Title property can only be modified by the class itself but can be read from outsite.
+        // Id and IsAvailable are sensitive, so only the class can read and update these properties.
+
+        public string Title { get; private set; }
+        private int Id { get; set; }
+        private bool IsAvailable { get; set; }
+
+        public Book (string title, int id)
+        {
+            Title = title;
+            Id = id;
+            IsAvailable = true;
+        }
+
+
+        public void Checkout()
+        {
+            if (!IsAvailable)
+            {
+                Console.WriteLine($"Book is already not available");
+                return;
+            }
+            IsAvailable = false;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Title: {Title}\nAvailable: {IsAvailable}");
+        }
+
+    }
+}
---
+++ BackendLessons/Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using Homework;
+
+Book MyFavoriteBook = new Book("Don Quijote", 0);
+
+Console.WriteLine("After being created:");
+MyFavoriteBook.ShowDetails();
+
+MyFavoriteBook.Checkout();
+
+Console.WriteLine("\nAfter checkout 1:");
+MyFavoriteBook.ShowDetails();
+
+Console.WriteLine($"\nTrying to checkout twice:");
+MyFavoriteBook.Checkout();
+
+Console.WriteLine("\nAfter checkout 2:");
+MyFavoriteBook.ShowDetails();
+
+
+// Title can be read from outside:
+//Console.WriteLine($"\nReading book title: {MyFavoriteBook.Title}");
+
+// Title cannot be set outsite of the class:
+//MyFavoriteBook.Title = "An invalid name"; // This line will throw a compilation error
+
+// The other properties cannot be read or set:
+// Console.WriteLine($"\nReading book Id: {MyFavoriteBook.Id}"); // This line will throw a compilation error
+// Console.WriteLine($"\nReading book IsAvailable: {MyFavoriteBook.IsAvailable}"); // This line will throw a compilation error
+// MyFavoriteBook.Id = 2; // This line will throw a compilation error
+// MyFavoriteBook.IsAvailable = true; // This line will throw a compilation error

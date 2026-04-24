dec48c3da6fdd68f154b77ef6b030f6e21656d0a
Edson-Quispe-jala <Edson.Quispe@jala.university>
2026-04-24 10:28:19 -0400

    add book class

---
+++ BackendLessons/ClassesAndAccessModifiers/Book.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassesAndAccessModifiers
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        private int Id { get; set; }
+        private bool IsAvailable { get; set; }
+
+        public Book(string title, int id)
+        {
+            Title = title;
+            Id = id;
+            IsAvailable = true;
+        }
+
+        public void Checkout()
+        {
+            IsAvailable = !IsAvailable;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Availability: {IsAvailable}");
+        }
+    }
+}
---
+++ BackendLessons/ClassesAndAccessModifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassesAndAccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name of the book:");
+            string bookName = Console.ReadLine() ?? "Book";
+
+            Book book = new Book(bookName, 1);
+
+            Console.WriteLine("Before make the checkout");
+            book.ShowDetails();
+            book.Checkout();
+
+            Console.WriteLine("\nAfter checkout:");
+            book.ShowDetails();
+        }
+    }
+}

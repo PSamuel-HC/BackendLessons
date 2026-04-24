5cf469d6698b7eeb272efaee1bd79013189ff27a
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-04-23 19:24:36 -0500

    Created Book class

---
+++ BackendLessons/BookClassExample/Book.cs
@@ -0,0 +1,28 @@
+
+/* "Other objects shouldn't be able to inherit nothing from this class"
+ So that's why is sealed*/
+sealed class Book
+{
+    private int _id; // sensible information
+    public string Title { get; set; }
+    private bool _isAvailable { get; set; } = true; // sensible information
+
+    public Book (string title, int id)
+    {
+        _id = id;
+        Title = title;
+    }
+
+    public void Checkout()
+    {
+        _isAvailable = false;
+    }
+
+    public void ShowDetails()
+    {
+        Console.WritleLine(
+            $"Book: {Title}," +
+            $"Availability: {_isAvailable}"
+            )
+    }
+}

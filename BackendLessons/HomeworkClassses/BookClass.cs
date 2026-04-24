ce806935e61861553f807ad62cb2195dd7bfe907
davidruizeJala <david.ruiz@jala.university>
2026-04-23 20:42:32 -0500

    Homework dome, create class and instantiate it

---
+++ BackendLessons/HomeworkClassses/BookClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkClassses
+{
+    sealed class BookClass
+    {
+
+        //the 3 Properties
+        public string Title { get; set; }
+        // private to protect it 
+        private int Id { get; set; }
+        // private to protect it 
+        private Boolean IsAvailable { get; set; }
+
+        // constructor that receives two arguments and set availability automatically
+        public BookClass(string title, int id) {
+            Title = title;
+            Id = id;
+            IsAvailable = true;
+        }
+
+        // first method requested
+        public void Checkout() {
+            if (IsAvailable)
+            {
+                IsAvailable = false;
+            }
+            else
+            {
+                Console.WriteLine("Book is already not available");
+            }
+        }
+
+        // I decided to add this extra method because it made sence jaja
+        public void CheckIn()
+        {
+            if (IsAvailable)
+            {
+                 Console.WriteLine("Book is already available");
+            }
+            else
+            {
+                IsAvailable = true;
+            }
+        }
+
+        //second method requested
+        public void ShowDetails()
+        {
+            Console.WriteLine("Title: " + Title + ", Is this book available? " + IsAvailable);
+        }
+    }
+}
---
+++ BackendLessons/HomeworkClassses/Program.cs
@@ -0,0 +1,28 @@
+﻿using HomeworkClassses;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //book instantiatedin the main program
+        BookClass book = new BookClass("100 años de soledad", 1);
+        //showed first book details
+        book.ShowDetails();
+
+        //confirmed checkout alert
+        book.Checkout();
+        book.Checkout();
+
+        //confirmed checkin alert
+        book.CheckIn();
+        book.CheckIn();
+
+        //final checkout
+        book.Checkout();
+
+        //display the details to verify the logic
+        book.ShowDetails();
+        
+    }
+
+}

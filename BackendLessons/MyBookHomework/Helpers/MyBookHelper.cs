0c9e00d6bc1fb05764619a58f2608bb0858ee425
RoBra18 <brand8n123@gmail.com>
2026-04-27 11:57:40 -0400

    Part 2 - Homework

---
+++ BackendLessons/MyBookHomework/Helpers/MyBookHelper.cs
@@ -0,0 +1,27 @@
+﻿using MyBookHomework.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBookHomework.Helpers
+{
+    internal static class MyBookHelper
+    {
+        /*
+         I decided to make the class static in order to avoid instantiating it every
+         time its functionality is needed. Furthermore, since it's a helper class,
+          the behavior of a non-static class is not usually necessary.
+         */
+        public static void GetAvailableBooksCount(List<Book> books)
+        {
+            int availableBooksCount = books.Where(x => x.IsAvailable).Count();
+            Console.WriteLine("Total available Books: " + availableBooksCount);
+        }
+        /*
+         Furthermore, I decided to keep this simple, given the nature of the task.
+         And, for better readability, I decided to use a WHERE clause to filter
+         the available books.
+         */
+
+    }
+}
--- BackendLessons/MyBookHomework/Program.cs
+++ BackendLessons/MyBookHomework/Program.cs
@@ -1,7 +1,9 @@
-﻿using MyBookHomework.Models;
+﻿using MyBookHomework.Helpers;
+using MyBookHomework.Models;
 
 try
 {
+    #region PART 1
     Console.WriteLine("My Book:");
 
     Book myBook = new Book(1, "Juan & Juanita");
@@ -14,8 +16,31 @@ try
     myBook.ShowDetails();
 
     //Verifying validation:
-    Console.WriteLine("Validation: ");
-    myBook.Checkout();
+    //Console.WriteLine("Validation: ");
+    //myBook.Checkout();
+    #endregion
+
+
+
+    #region PART 2
+
+    //1. ARRAY of Genres:
+    string[] genres = new string[] { "Comedy", "Action", "Drama"};
+
+    //List of books
+    List<Book> books = new List<Book>() { new Book (1, "Juan y Juana"), 
+        new Book(1, "Pepito y Juanita"),
+        new Book(1, "Pedrito returns"), 
+        new Book(1, "Coquito"), };
+
+    //I checked out a book to verify that the Helper works.
+    books[0].Checkout();
+
+    //Helper entity:
+    MyBookHelper.GetAvailableBooksCount(books);
+
+
+    #endregion
 }
 
 catch (InvalidOperationException ex)

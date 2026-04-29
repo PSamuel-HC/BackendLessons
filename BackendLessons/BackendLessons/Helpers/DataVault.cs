1f3b4a77258bd2268d8ac3df42b9d2c721ab3edd
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-04-28 19:31:33 -0500

    feat: creating DataVault class and adding AddItem method

---
+++ BackendLessons/BackendLessons/Helpers/DataVault.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jalasoft.GoldenRecord.Helpers
+{
+    internal class DataVault<T> where T : class, IEntity, new()
+    {
+        private List<T> itemsList { get; set; } = new List<T>();
+
+        public void AddItem(T item)
+        {
+            Console.WriteLine($"Adding item with id: {item.Id}");
+            itemsList.Add(item);
+        }
+    }
+}
--- BackendLessons/BackendLessons/Program.cs
+++ BackendLessons/BackendLessons/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Jalasoft.GoldenRecord;
+using Jalasoft.GoldenRecord.Helpers;
 using Jalasoft.GoldenRecord.Models;
 using System.Collections;
 
@@ -10,17 +11,27 @@ namespace BackendLessons
         {
             User user1 = new User();
             User user2 = new User(Guid.NewGuid(), "Juan");
+            User user3 = new User(Guid.NewGuid(), "Jose");
 
-            Product product1 = new Product { };
-            Product product2 = new Product { };
+            DataVault<User> userVault = new DataVault<User>();
 
-            Console.WriteLine($"This is user1 guid: {user1.Id}\n" +
-                $"and this is product1 guid: {product1.Id}");
+            userVault.AddItem(user1);
+            userVault.AddItem(user2);
+            userVault.AddItem(user3);
 
-            Console.WriteLine($"This is user2 guid: {user2.Id}\n" +
-                $"and user2 name: {user2.Username}");
 
-            Console.WriteLine($"This is product1 price: {product1.Price}");
+            // * TEST CODE - IGNORE
+
+            //Product product1 = new Product { };
+            //Product product2 = new Product { };
+
+            //Console.WriteLine($"This is user1 guid: {user1.Id}\n" +
+            //    $"and this is product1 guid: {product1.Id}");
+
+            //Console.WriteLine($"This is user2 guid: {user2.Id}\n" +
+            //    $"and user2 name: {user2.Username}");
+
+            //Console.WriteLine($"This is product1 price: {product1.Price}");
 
         }
     }

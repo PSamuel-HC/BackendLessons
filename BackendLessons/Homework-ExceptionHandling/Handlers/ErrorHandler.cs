de97629a22b0a2af109f5d02f7cc8ee805195608
Luigi-Jala <luigi.cabrera@jala.university>
2026-04-30 10:15:23 -0500

    Base Exception Handling Structure

---
+++ BackendLessons/Homework-ExceptionHandling/Handlers/ErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling.Handlers
+{
+    internal abstract class ErrorHandler : IErrorHandler
+    {
+        public abstract void Handle(ErrorHandlerContext context);
+
+        public void NotFound()
+        {
+            Console.WriteLine("Element not found");
+        }
+
+        public void BadRequest()
+        {
+            Console.WriteLine("Bad Request");
+        }
+    }
+}

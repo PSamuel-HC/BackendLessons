49f928a78e8459c1f5e9559307f571e047fd8bf6
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-05-05 17:46:21 -0500

    feat: create Employee model

---
+++ MyStore.Domain/Model/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Domain.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Rol { get; set; } = string.Empty;
+        public decimal HourlyRate { get; set; }
+        public DateTime HireDate { get; set; }
+
+    }
+}

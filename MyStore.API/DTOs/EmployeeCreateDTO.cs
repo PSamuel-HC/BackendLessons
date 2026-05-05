ad8585d6e32f8a81b753a758e541dfa075533ef4
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-05-05 18:15:54 -0500

    feat: created controller for employee

--- MyStore.API/DTOs/EmployeeCreateDTO.cs
+++ MyStore.API/DTOs/EmployeeCreateDTO.cs
@@ -6,9 +6,10 @@ namespace MyStore.Domain.Model
 {
     public class EmployeeCreateDTO
     {
-        public int Id { get; set; }
-        public string FullName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public decimal HourlyRate { get; set; }
         public DateTime HireDate { get; set; }
 
     }

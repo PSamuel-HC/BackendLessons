42e8d4e6e6c8d39e60852f7cdc9b0fcd134ab7f5
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-05-05 18:01:57 -0500

    feat: created EmployeeReadDTO

--- MyStore.API/DTOs/EmployeeReadDTO.cs
+++ MyStore.API/DTOs/EmployeeReadDTO.cs
@@ -1,16 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MyStore.Domain.Model
 {
-    public class Employee
+    public class EmployeeReadDTO
     {
         public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
-        public decimal HourlyRate { get; set; }
         public DateTime HireDate { get; set; }
 
     }

dc34cd9a03d862b386afbd588ec2922f16ce884f
Edson-Quispe-jala <Edson.Quispe@jala.university>
2026-05-06 10:36:40 -0400

    add order model and dto

---
+++ MyStore.API/DTOs/OrderReadDto.cs
@@ -0,0 +1,15 @@
+﻿using MyStore.Domain.Emuns;
+
+namespace MyStore.API.DTOs
+{
+    public class OrderReadDto
+    { 
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public OrderStatus Status { get; set; }
+        public string ShippingAddress { get; set; }
+        public DateTime EstimatedDelivery { get; set; } = DateTime.UtcNow.AddDays(7);
+    }
+}
---
+++ MyStore.Domain/Model/Order.cs
@@ -0,0 +1,17 @@
+﻿using MyStore.Domain.Emuns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Domain.Model
+{
+    public class Order
+    {
+        public int Id { get; set; } // The Primary Key
+        public string OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public OrderStatus Status { get; set; }
+        public string ShippingAddress { get; set; }
+    }
+}

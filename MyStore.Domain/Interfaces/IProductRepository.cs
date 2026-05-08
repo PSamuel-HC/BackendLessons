cd2d64fdfb5bb46d300dace684cde7554cc4000d
davidruizeJala <david.ruiz@jala.university>
2026-05-07 20:46:14 -0500

    feature: Create layers

---
+++ MyStore.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyStore.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetById(int id);
+        Task Create(Product product);
+        Task Update(Product product);
+        Task Delete(Product product);
+    }
+}
---
+++ MyStore.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using MyStore.Domain.Interfaces;
+using MyStore.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Text;
+
+namespace MyStore.Infrastructure.Repositories
+{
+    internal class ProductRepository(MyStoreDbContext context) : IProductRepository
+    {
+        public async Task Create(Product product)
+        {
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Product product)
+        {
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetById(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+        public async Task Update(Product product)
+        {
+           context.Products.Update(product);
+           await context.SaveChangesAsync();
+        }
+    }
+}

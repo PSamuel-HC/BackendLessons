fce56d802fe4d064019cb55da6ea8ed3c5499305
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-05-08 09:12:36 -0500

    feat: add customer repo to infra and interface to domain

---
+++ MyStore.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyStore.Domain.Model;
+
+namespace MyStore.Domain.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer> GetOneAsync();
+        Task<Customer> CreateAsync();
+        void UpdateAsync();
+        void DeleteAsync();
+    }
+}
---
+++ MyStore.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using MyStore.Domain.Interfaces;
+using MyStore.Domain.Model;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Infrastructure.Repositories
+{
+    internal class CustomerRepository : ICustomerRepository
+    {
+        public Task<Customer> CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetOneAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
